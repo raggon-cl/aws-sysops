e3d7aba13ba77dbddac11ca94e920fdb2a7388a7
Rodrigo Aguilar <raggon@gmail.com>
2023-06-05 15:18:48 -0400

    Update lab06-userdata

--- lab06-userdata.sh
+++ lab06-userdata.sh
@@ -3,6 +3,6 @@
 amazon-linux-extras install epel -y
 yum install httpd wget git unzip stress -y
 systemctl restart httpd
-curl -O https://www.tooplate.com/zip-templates/2121_wave_cafe.zip
+curl -O https://github.com/raggon-cl/aws-sysops/raw/main/lab06_2121_wave_cafe.zip
 unzip 2121_wave_cafe.zip
 mv 2121_wave_cafe/* /var/www/html
